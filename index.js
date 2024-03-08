29a1b0800c1bcf73ba00f8c03aec87e765f0e487
umang <106922225+umang-soni@users.noreply.github.com>
2024-03-08 21:52:58 +0530

    my first commit in version1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-
-const qrcode = require('qrcode-terminal');
+const fs = require('fs');
+const qrcode = require('qrcode');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 require('dotenv').config();
 
@@ -9,16 +9,21 @@ const whatsapp = new Client({
 
 let isInitialized = false; // Flag to track initialization status
 
-whatsapp.on('qr', qr => {
-    qrcode.setErrorLevel('M');
-    qrcode.generate(qr, { small: true });
-    console.log("Scan the QR code above with your phone's WhatsApp.");
+whatsapp.on('qr', async qr => {
+    try {
+        const qrCodeImage = await qrcode.toDataURL(qr);
+        console.log("Scan the QR code above with your phone's WhatsApp.");
+        console.log(qr); // Print the QR code to console (optional)
+        // Save the QR code image to a file (optional)
+        fs.writeFileSync('qrcode.png', qrCodeImage.split(';base64,').pop(), { encoding: 'base64' });
+    } catch (error) {
+        console.error('Error generating QR code:', error);
+    }
 });
 
 whatsapp.on('ready', () => {
     console.log("Client is ready");
 });
-
 
 whatsapp.on('authenticated', (session) => {
     console.log('Authenticated');
@@ -44,4 +49,5 @@ if (!isInitialized) {
             console.error('Initialization error:', err);
         });
 }
+
 
