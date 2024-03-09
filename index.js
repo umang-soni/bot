a3b2f7b1e3e2373553f943a83ecc6c87095b2f08
umang <106922225+umang-soni@users.noreply.github.com>
2024-03-09 15:34:53 +0530

    my first commit  in version2

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const qrcode = require('qrcode-terminal');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const express=require("express")
 const app= express();
+const mongoose=require('mongoose');
+require('dotenv').config();
+console.log(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+    console.log("database is connected successfully")
+
+}).catch((error)=>{
+    console.log("error in connected database",error)
+})
+
+
+
+
+
+
 const port=process.env.PORT||5000;
 
 const whatsapp = new Client({
